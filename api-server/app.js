2fb4cca9702fc05c32dd7779d3bfd6dc6f533760
Aviad Shikloshi <aviad@perimeterx.com>
2018-05-01 09:35:19 +0300

    log hostname in api_server

--- api-server/app.js
+++ api-server/app.js
@@ -1,9 +1,10 @@
 const http = require('http');
+const os = require('os');
 
 const port = process.env.PORT || '8080';
 
 http.createServer((req, res) => {
-    console.log("Got new request")
+    console.log(`${os.hostname()} Got new request`)
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.write(JSON.stringify({ method: req.method }));
     res.end();
